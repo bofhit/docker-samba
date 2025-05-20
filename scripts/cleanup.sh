1698b641cec4cd73d323bf0ccc664bdb659be778
Tim Burkholder <it@blessingsofhope.com>
2025-05-20 17:40:20 +0000

    Set mtime.

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -2,5 +2,5 @@
 
 # Remove files from shared folder.
 echo $(date +%Y-%m-%dT%H:%M:%S)
-find /srv/share -mtime +1 -type f ! -name README.txt -print0 | xargs -0 rm
+find /srv/share -mtime +30 -type f ! -name README.txt -print0 | xargs -0 rm
 
