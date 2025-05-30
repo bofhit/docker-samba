18343ee0e5602fac3394ad4771443c2351c6ad5c
Tim Burkholder <it@blessingsofhope.com>
2025-05-30 17:47:39 +0000

    Using auto generated conf files.

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -3,6 +3,12 @@
 service cron start
 service nmbd start
 service smbd start
+service sssd start
+
+# sssd needs the following:
+touch /etc/sssd/sssd.conf
+chown root:root /etc/sssd/sssd.conf
+chmod 600 /etc/sssd/sssd.conf
 
 # Keep alive after starting service(s).
 sleep infinity
