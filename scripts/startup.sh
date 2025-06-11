a58ac94606d927d8b1843305364eae525756165b
Tim Burkholder <it@blessingsofhope.com>
2025-06-11 16:53:18 +0000

    Add share user.

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -4,6 +4,13 @@ service cron start
 service nmbd start
 service smbd start
 
+useradd -m $SHARE_USER
+echo "$SHARE_USER:$SHARE_USER_PASSWD" | chpasswd
+
+# Add Samba user.
+echo -e "$SHARE_USER_PASSWD\n$SHARE_USER_PASSWD" | \
+    smbpasswd -a -s $SHARE_USER
+
 # Keep alive after starting service(s).
 sleep infinity
 
