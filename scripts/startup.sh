93ea11f4161f7b535e75f47dba2bdc31212b5259
Tim Burkholder <it@blessingsofhope.com>
2025-06-18 18:31:10 +0000

    Modified config.

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -11,6 +11,9 @@ echo "$SHARE_USER:$SHARE_USER_PASSWD" | chpasswd
 echo -e "$SHARE_USER_PASSWD\n$SHARE_USER_PASSWD" | \
     smbpasswd -a -s $SHARE_USER
 
+sed -i "s/^\s*write list = .*/    write list = $SHARE_USER/" /etc/samba/smb.conf
+sed -i "s/^\s*valid users = .*/    valid users = $SHARE_USER/" /etc/samba/smb.conf
+
 # Keep alive after starting service(s).
 sleep infinity
 
